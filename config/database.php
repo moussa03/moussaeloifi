51ef5c944cb341f587ce421e7b7047dbcc919979
moussa03 <50172886+moussa03@users.noreply.github.com>
2023-01-27 19:25:40 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -58,11 +58,11 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+          /*  'options' => extension_loaded('pdo_mysql') ? array_filter([
                 // PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 // PDO::MYSQL_ATTR_SSL_CA => '/ssl/cacert.pem',
                 PDO::MYSQL_ATTR_SSL_CERT => '/ssl/cacert.pem',
-            ]) : [],
+            ]) : [],*/
         ],
 
         'pgsql' => [
